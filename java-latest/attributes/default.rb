6900c11882989e879243d5cc2f72f9e7a437f87c
xezer <xezer@tut.by>
2013-11-28 12:15:35 +0300

    no message

--- java-latest/attributes/default.rb
+++ java-latest/attributes/default.rb
@@ -38,18 +38,18 @@ end
 
 # jdk6 attributes
 # x86_64
-default['java']['jdk']['6']['x86_64']['url'] = 'http://chef.htcblr.by/files/java/jdk-6u45-linux-x64.bin'
+default['java']['jdk']['6']['x86_64']['url'] = 'http://107.20.108.96/files/java/jdk-6u45-linux-x64.bin'
 default['java']['jdk']['6']['x86_64']['checksum'] = '6b493aeab16c940cae9e3d07ad2a5c5684fb49cf06c5d44c400c7993db0d12e8'
 
 # i586
-default['java']['jdk']['6']['i586']['url'] = 'http://chef.htcblr.by/files/java/jdk-6u45-linux-i586.bin'
+default['java']['jdk']['6']['i586']['url'] = 'http://107.20.108.96/files/java/jdk-6u45-linux-i586.bin'
 default['java']['jdk']['6']['i586']['checksum'] = 'd53b5a2518d80e1d95565f0adda54eee229dc5f4a1d1a3c2f7bf5045b168a357'
 
 # jdk7 attributes
 # x86_64
-default['java']['jdk']['7']['x86_64']['url'] = 'http://chef.htcblr.by/files/java/jdk-7u40-linux-x64.tar.gz'
+default['java']['jdk']['7']['x86_64']['url'] = 'http://107.20.108.96/files/java/jdk-7u40-linux-x64.tar.gz'
 default['java']['jdk']['7']['x86_64']['checksum'] = '72f6e010592cad5e994276eee7db5f0b0d7c15c06949bd81f0e14811048bcf2c'
 
 # i586
-default['java']['jdk']['7']['i586']['url'] = 'http://chef.htcblr.by/files/java/jdk-7u40-linux-i586.tar.gz'
+default['java']['jdk']['7']['i586']['url'] = 'http://107.20.108.96/files/java/jdk-7u40-linux-i586.tar.gz'
 default['java']['jdk']['7']['i586']['checksum'] = '3a70b0c41cdf089d423c28336495c3e205f4fcb34f538dd82b80044eba670e8b'
